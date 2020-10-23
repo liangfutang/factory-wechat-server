f23662717087ecfb12c39b7b0714bdea1f6c2633
liangfutang <liangfu.tang@ecarx.com.cn>
2020-10-24 00:57:26 +0800

    修改消息体的Data注解

--- biz/src/main/java/com/zjut/factory/server/biz/manage/entity/msg/BaseMessage.java
+++ biz/src/main/java/com/zjut/factory/server/biz/manage/entity/msg/BaseMessage.java
@@ -4,15 +4,12 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlCData;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
 import java.util.Map;
 
-@Setter@Getter@ToString
+@Data
 @JacksonXmlRootElement(localName = "xml")
-public class BaseMessage {
+public abstract class BaseMessage {
 
 	@JacksonXmlCData
 	@JacksonXmlProperty(localName = "ToUserName")
@@ -20,7 +17,6 @@ public class BaseMessage {
 	@JacksonXmlCData
 	@JacksonXmlProperty(localName = "FromUserName")
 	private String fromUserName;
-//	@JacksonXmlCData
 	@JacksonXmlProperty(localName = "CreateTime")
 	private String createTime;
 	/**
@@ -30,7 +26,7 @@ public class BaseMessage {
 	@JacksonXmlProperty(localName = "MsgType")
 	private String msgType;
 
-	public BaseMessage(Map<String, String> requestMap, String msgType) {
+	BaseMessage(Map<String, String> requestMap, String msgType) {
 		this.toUserName=requestMap.get("FromUserName");
 		this.fromUserName=requestMap.get("ToUserName");
 		this.createTime=System.currentTimeMillis()/1000+"";
--- biz/src/main/java/com/zjut/factory/server/biz/manage/entity/msg/TextMessage.java
+++ biz/src/main/java/com/zjut/factory/server/biz/manage/entity/msg/TextMessage.java
@@ -5,18 +5,15 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlCData;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import com.zjut.factory.server.common.constants.CommConstants;
-import lombok.*;
 
 import java.util.Map;
 
-@Setter@Getter@ToString
 @JacksonXmlRootElement(localName = "xml")
 public class TextMessage extends BaseMessage {
 
 	@JacksonXmlCData
 	@JacksonXmlProperty(localName = "Content")
 	private String content;
-//	@JacksonXmlCData
 	@JacksonXmlProperty(localName = "MsgId")
 	private String msgId;
 
